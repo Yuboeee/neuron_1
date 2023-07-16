4dc53060b7c8bc34fb489d147f9445fd2e3def96
Yuboeee <wuyubo.elise@outlook.com>
2023-07-16 15:14:05 +0100

    change neuron.v

--- source/neuron.v
+++ source/neuron.v
@@ -7,7 +7,7 @@ module neuron #(
     input clk,
     input rst,
     input [dataWidth-1:0] myInput,
-//    input myinputValid,
+    input myinputValid,
 //    input weightValid,
 //    input biasValid,
     input [31:0] weightValue,
@@ -21,7 +21,7 @@ module neuron #(
     parameter addressWidth = $clog2(numWeight);
 
     reg wen;
-//    wire        ren;
+    wire        ren;
     reg [addressWidth-1:0] w_addr;
     reg [addressWidth:0]   r_addr;//read address has to reach until numWeight hence width is 1 bit more
     reg [dataWidth-1:0]  w_in;
@@ -49,7 +49,8 @@ module neuron #(
             w_addr <= {addressWidth{1'b1}};
             wen <=0;
         end
-        else if(weightValid & (config_layer_num==layerNo) & (config_neuron_num==neuronNo))
+        //else if(weightValid & (config_layer_num==layerNo) & (config_neuron_num==neuronNo))
+        else if((config_layer_num == layerNo) & (config_neuron_num == neuronNo))
         begin
             w_in <= weightValue;
             w_addr <= w_addr + 1;
@@ -59,6 +60,45 @@ module neuron #(
             wen <= 0;
     end
 
+    assign mux_valid = mult_valid;
+    assign comboAdd = mul + sum;
+//    assign BiasAdd = bias + sum;
+    assign ren = myinputValid;
+
+    always @(posedge clk)
+    begin
+        if(rst|outvalid)
+            r_addr <= 0;
+        else if(myinputValid)
+            r_addr <= r_addr + 1;
+    end
+
+    always @(posedge clk)
+    begin
+        mul  <= $signed(myinputd) * $signed(w_out);
+    end
+
+    always @(posedge clk)
+    begin
+        myinputd <= myinput;
+        weight_valid <= myinputValid;
+        mult_valid <= weight_valid;
+        sigValid <= ((r_addr == numWeight) & muxValid_f) ? 1'b1 : 1'b0;
+        outvalid <= sigValid;
+        muxValid_d <= mux_valid;
+        muxValid_f <= !mux_valid & muxValid_d;
+    end
+    
+    //Instantiation of Memory of Weights
+    Weight_Memory #(.numWeight(numWeight),.addressWidth(addressWidth),.dataWidth(dataWidth),.weightFile(weightFile)) WM(
+        .clk(clk),
+    //    .wen(wen),
+        .ren(ren),
+        .wadd(w_addr),
+        .radd(r_addr),
+        .win(w_in),
+        .wout(w_out)
+    );
 
 
 
